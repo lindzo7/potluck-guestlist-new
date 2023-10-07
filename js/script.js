4d92b503876a4fae96d4213574db440cc6a7f573
Lindsey Jarrett <lindsey.jarrett7@gmail.com>
2023-10-07 12:20:50 -0400

    added event listener to add guests

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value; 
+    if (guest !== "") {
+        let listItem = document.createElement("li");
+        listItem.innerText = guest;
+        guestList.append(listItem);
+    }
+});
+
